d7c86cd457ba4c0f6eae0c7d20a80aac643fe82a
pedro.mujica1 <miottopedro@gmail.com>
2025-03-14 19:57:59 -0300

    ciclos done

--- Neander/UC/cicloJz.vhdl
+++ Neander/UC/cicloJz.vhdl
@@ -9,18 +9,31 @@ entity cicloJz is
 end entity cicloJz;
 
 architecture arch of cicloJz is
+    component cicloJmp is
+        port(
+            counter : in std_logic_vector(2 downto 0);
+            s : out std_logic_vector(10 downto 0);
+            );
+    end component cicloJmp;
+    signal  jmpTrue, jmpFalse : std_logic_vector(10 downto 0);
 
 begin
 
-    s(10) <=  '0'; --barrINC
-    s(9) <= '0' --barrPC
-    s(8 downto 6) <= "000"; --ULA_OP altera pra 
-    s(5) <= '0'; --PC_NRW
-    s(4) <= '0'; --AC_NRW
-    s(3) <= '0'; --MEM_NRW
-    s(2) <= '0' --rem_nrw
-    s(1) <= '0'  --RDM_NRW
-    s(0) <= '0'; --RI_NRW
+    u_jmp : ciclo_jmp port map(counter,jmpTrue);
     
+    jmpFalse(10) <=  '1'; --barrINC
+    jmpFalse(9) <= '1' --barrPC
+    jmpFalse(8 downto 6) <= "000"; --ULA_OP altera pra 
+    jmpFalse(5) <= not(counter(2)) and counter(0); --PC_NRW
+    jmpFalse(4) <= '0'; --AC_NRW
+    jmpFalse(3) <= '0'; --MEM_NRW
+
+    jmpFalse(2) <= not(counter(0)) and not(counter(1)) and not(counter(2)); --rem_nrw
+    jmpFalse(1) <=  counter(0) and not(counter(1)) and not(counter(2)) --RDM_NRW
+    jmpFalse(0) <= counter(1) and not(counter(0)) and not(counter(2)); --RI_NRW
+    
+    s <= jmpTrue when nz(1) = '1' else jmpFalse;
+    --s <= jmpTrue when nz(1) = '1' else jmpFalse;
+
 
 end arch;
