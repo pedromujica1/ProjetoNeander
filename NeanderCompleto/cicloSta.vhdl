1ffdf085c92dbf037d48dea7ff605270308e76f4
pedro.mujica1 <miottopedro@gmail.com>
2025-03-20 20:17:24 -0300

    FINISHEDDD

--- NeanderCompleto/cicloSta.vhdl
+++ NeanderCompleto/cicloSta.vhdl
@@ -20,7 +20,7 @@ begin
     -- 
     s(3)<=  counter(1) and not(counter(0)) and counter(2);--MEM_NRW
     s(2)<= ( not(counter(1)) and (counter(2) xnor counter(0)) ) or (counter(0) and counter(1) and not counter(2));--rem_nrw
-    s(1)<= not(counter(1)) and (counter(2) xor counter(2));--RDM_NRW --n
+    s(1)<= not(counter(1)) and (counter(0) xor counter(2));--RDM_NRW --n
     --aaaa 
     s(0) <= not counter(0) and counter(1) and not counter(2); --RI_NRW mantem igual
 end arch;
